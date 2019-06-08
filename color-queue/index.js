4203477c51107ae6abef7bd33df2deb305390641
nodebotanist <the@nodebotanist>
2019-06-08 23:15:30 +0100

    Color Queue checks JWT and parses colors

--- color-queue/index.js
+++ color-queue/index.js
@@ -1,3 +1,7 @@
+const jwt = require('jsonwebtoken')
+const secrets = require('./secrets.js')
+const color = require('color')
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -7,5 +11,42 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', { status: 200 })
+  let userJWT = request.headers.get('Authorization')
+  if(!userJWT) {
+    return new Response('Need a JWT in your Authorization header', {status: 403})
+  }
+  userJWT = userJWT.replace(/^Bearer /, '')
+  try {
+    let decoded = jwt.verify(userJWT, secrets.JWT_SECRET_KEY)
+    let userColor, colorType
+    // key works, off we go!
+    if (decoded.color) {
+      colorType = 'CSS'
+    } else if (decoded.h || decoded.s || decoded.v) {
+      colorType = 'HSV'
+    } else if (decoded.r || decoded.g || decoded.b) {
+      colorType = 'RGB'
+    } else {
+      console.log(decoded)
+      return new Response ('Invalid color in JWT ', {status: 400})
+    }
+    try {
+      switch(colorType) {
+        case 'CSS':
+          userColor = color(decoded.color)
+          break
+        case 'HSV':
+          userColor = color({h: decoded.h, s: decoded.s, v: decoded.v})
+          break
+        case 'RGB':
+          userColor = color({r: decoded.r, g: decoded.g, b:decoded.b})
+      }
+    } catch (err) {
+      return new Response('Inwalid color: ' + err, {status: 400})
+    }
+    return new Response(userColor.toString(), {status: 200})
+  } catch (err) {
+    return new Response(err, {status: 403})
+  }
 }
+
--- jwt-dispenser/index.js
+++ jwt-dispenser/index.js
@@ -1,5 +1,5 @@
-const jwt = require("jsonwebtoken")
-const secrets = require('./secrets')
+const jwt = require("jsonwebtoken");
+const secrets = require("./secrets");
 
 addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request));
