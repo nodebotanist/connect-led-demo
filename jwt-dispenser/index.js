9e14bcaf385ee2742b58a22aa7d64a0e556581e7
nodebotanist <the@nodebotanist>
2019-06-08 16:04:36 +0100

    Getting secrets set up

--- jwt-dispenser/index.js
+++ jwt-dispenser/index.js
@@ -1,11 +1,16 @@
+const jwt = require('jsonwebtoken')
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
 /**
- * Fetch and log a request
+ * when a request comes in, dispense a jwt signed with an ENV 
+ * variable
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', { status: 200 })
+  require('dotenv').config()
+  let token = jwt.sign({demo: 'connect-color'}, process.env.JWT_SECRET_KEY)
+  return new Response(token, { status: 200 })
 }
