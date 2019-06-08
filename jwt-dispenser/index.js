24ee3e0c4503c963e8e746d6e2bfd11e4328c0ca
nodebotanist <the@nodebotanist>
2019-06-08 16:09:28 +0100

    JWT dispenser works

--- jwt-dispenser/index.js
+++ jwt-dispenser/index.js
@@ -1,16 +1,16 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken")
+const secrets = require('./secrets')
 
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
 
 /**
- * when a request comes in, dispense a jwt signed with an ENV 
+ * when a request comes in, dispense a jwt signed with an ENV
  * variable
  * @param {Request} request
  */
 async function handleRequest(request) {
-  require('dotenv').config()
-  let token = jwt.sign({demo: 'connect-color'}, process.env.JWT_SECRET_KEY)
-  return new Response(token, { status: 200 })
+  let token = jwt.sign({ demo: "connect-color" }, secrets.JWT_SECRET_KEY);
+  return new Response(token, { status: 200 });
 }
