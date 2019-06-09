8527608892101c1616f7390eb9d48dd48ca290cd
nodebotanist <the@nodebotanist>
2019-06-09 21:31:37 +0100

    Dispenser works

--- jwt-dispenser/index.js
+++ jwt-dispenser/index.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const secrets = require("./secrets");
-const Router = require('./router')
+const Router = require("./router");
 
 addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request));
@@ -12,16 +12,33 @@ addEventListener("fetch", event => {
  * @param {Request} request
  */
 
- let r = new Router()
+let r = new Router();
 
 async function handleRequest(request) {
-  r.post('.*/', (request) => generateToken(request))
+  r.post(".*/", request => generateToken(request));
 
-  const resp = await r.route(request)
-  return resp
+  const resp = await r.route(request);
+  return resp;
 }
 
 async function generateToken(request) {
-  let token = jwt.sign({ demo: "connect-color" }, secrets.JWT_SECRET_KEY);
-  return new Response(token, {status: 200})
+  let body;
+  try {
+    body = await request.json();
+  } catch {
+    return new Response("invalid JSON in request body", { status: 400 });
+  }
+  if (
+    !body.color &&
+    ((!body.r && !body.r === 0) ||
+      (!body.g && !body.g === 0) ||
+      (!body.b && !body.b === 0)) &&
+    ((!body.h && !body.h === 0) ||
+      (!body.s && !body.s === 0) ||
+      (!body.v && !body.v === 0))
+  ) {
+    return new Response("No valid color in payload", { status: 400 });
+  }
+  let token = jwt.sign(body, secrets.JWT_SECRET_KEY);
+  return new Response(token, { status: 200 });
 }
--- jwt-dispenser/router.js
+++ jwt-dispenser/router.js
@@ -3,24 +3,25 @@
  * will return a boolean for if that request uses
  * that method, header, etc..
  *  */
-const Method = method => req => req.method.toLowerCase() === method.toLowerCase()
-const Get = Method('get')
-const Post = Method('post')
-const Put = Method('put')
-const Patch = Method('patch')
-const Delete = Method('delete')
-const Head = Method('patch')
-const Options = Method('options')
+const Method = method => req =>
+  req.method.toLowerCase() === method.toLowerCase();
+const Get = Method("get");
+const Post = Method("post");
+const Put = Method("put");
+const Patch = Method("patch");
+const Delete = Method("delete");
+const Head = Method("patch");
+const Options = Method("options");
 
-const Header = (header, val) => req => req.headers.get(header) === val
-const Host = host => Header('host', host.toLowerCase())
-const Referrer = host => Header('referrer', host.toLowerCase())
+const Header = (header, val) => req => req.headers.get(header) === val;
+const Host = host => Header("host", host.toLowerCase());
+const Referrer = host => Header("referrer", host.toLowerCase());
 
 const Path = regExp => req => {
-  const url = new URL(req.url)
-  const path = url.pathname
-  return path.match(regExp) && path.match(regExp)[0] === path
-}
+  const url = new URL(req.url);
+  const path = url.pathname;
+  return path.match(regExp) && path.match(regExp)[0] === path;
+};
 
 /**
  * Router handles the logic of what handler is matched given conditions
@@ -28,51 +29,51 @@ const Path = regExp => req => {
  *  */
 class Router {
   constructor() {
-    this.routes = []
+    this.routes = [];
   }
 
   handle(conditions, handler) {
     this.routes.push({
       conditions,
-      handler,
-    })
-    return this
+      handler
+    });
+    return this;
   }
 
   get(url, handler) {
-    return this.handle([Get, Path(url)], handler)
+    return this.handle([Get, Path(url)], handler);
   }
 
   post(url, handler) {
-    return this.handle([Post, Path(url)], handler)
+    return this.handle([Post, Path(url)], handler);
   }
 
   patch(url, handler) {
-    return this.handler([Patch, Path(url)], handler)
+    return this.handler([Patch, Path(url)], handler);
   }
 
   delete(url, handler) {
-    return this.handler([Delete, Path(url)], handler)
+    return this.handler([Delete, Path(url)], handler);
   }
 
   all(handler) {
-    return this.handler([], handler)
+    return this.handler([], handler);
   }
 
   route(req) {
-    const route = this.resolve(req)
+    const route = this.resolve(req);
 
     if (route) {
-      return route.handler(req)
+      return route.handler(req);
     }
 
-    return new Response('resource not found', {
+    return new Response("resource not found", {
       status: 404,
-      statusText: 'not found',
+      statusText: "not found",
       headers: {
-        'content-type': 'text/plain',
-      },
-    })
+        "content-type": "text/plain"
+      }
+    });
   }
 
   // resolve returns the matching route that returns true for
@@ -80,16 +81,16 @@ class Router {
   resolve(req) {
     return this.routes.find(r => {
       if (!r.conditions || (Array.isArray(r) && !r.conditions.length)) {
-        return true
+        return true;
       }
 
-      if (typeof r.conditions === 'function') {
-        return r.conditions(req)
+      if (typeof r.conditions === "function") {
+        return r.conditions(req);
       }
 
-      return r.conditions.every(c => c(req))
-    })
+      return r.conditions.every(c => c(req));
+    });
   }
 }
 
-module.exports = Router
+module.exports = Router;
