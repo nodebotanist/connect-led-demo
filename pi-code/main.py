bdb789c4d42429265c836bdd1f57f6d1beef5882
nodebotanist <the@nodebotanist>
2019-06-09 20:55:26 +0100

    It works!

--- pi-code/main.py
+++ pi-code/main.py
@@ -1,12 +1,28 @@
 import unicornhathd
 import urllib3
+import time
+import json
 
-unicornhathd.set_pixel(0,0,255,0,0)
-unicornhathd.set_pixel(0,1,0,255,0)
-unicornhathd.set_pixel(1,0,0,0,255)
-unicornhathd.set_pixel(1,1,255,255,255)
+urllib3.disable_warnings()
+http = urllib3.PoolManager()
 
-unicornhathd.show()
+internalBuffer = []
 
 while True:
+	r = http.request('GET', 'http://color-queue.kas.workers.dev/get-colors', headers={'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2xvciI6ImJsdWUifQ.IYQvIrX4Igk8zogYwIinHxJmDva213hrUUn0tBTjdhM', 'Accept': '*/*'})
+	data = json.loads(r.data.decode('utf-8'))
+	for color in data:
+		internalBuffer.insert(0, color['color'])
+	if len(internalBuffer) >= 255: internalBuffer = internalBuffer[0-255]
+	x = 0
+	y = 0
+	index = 0
+	for color in internalBuffer:
+		unicornhathd.set_pixel(x, y, internalBuffer[index][0],  internalBuffer[index][1],  internalBuffer[index][2])
+		x += 1
+		index +=1
+		if x > 15: 
+			x = 0
+			y += 1
 	unicornhathd.show()
+	time.sleep(5)
