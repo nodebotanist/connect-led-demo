49e0962553ffbea24683af4b3644a2ff5b674d16
nodebotanist <the@nodebotanist>
2019-06-09 15:56:58 +0100

    Router added to color queue

--- color-queue/index.js
+++ color-queue/index.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken')
-const secrets = require('./secrets.js')
 const color = require('color')
+
+const secrets = require('./secrets.js')
+const Router = require('./router')
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
@@ -16,37 +18,50 @@ async function handleRequest(request) {
     return new Response('Need a JWT in your Authorization header', {status: 403})
   }
   userJWT = userJWT.replace(/^Bearer /, '')
+
+  const r = new Router()
+  // r.get('.*/bar', () => new Response('responding for /bar'))
+  // r.get('.*/foo', req => handler(req))
+
   try {
     let decoded = jwt.verify(userJWT, secrets.JWT_SECRET_KEY)
-    let userColor, colorType
     // key works, off we go!
-    if (decoded.color) {
-      colorType = 'CSS'
-    } else if (decoded.h || decoded.s || decoded.v) {
-      colorType = 'HSV'
-    } else if (decoded.r || decoded.g || decoded.b) {
-      colorType = 'RGB'
-    } else {
-      console.log(decoded)
-      return new Response ('Invalid color in JWT ', {status: 400})
-    }
-    try {
-      switch(colorType) {
-        case 'CSS':
-          userColor = color(decoded.color)
-          break
-        case 'HSV':
-          userColor = color({h: decoded.h, s: decoded.s, v: decoded.v})
-          break
-        case 'RGB':
-          userColor = color({r: decoded.r, g: decoded.g, b:decoded.b})
-      }
-    } catch (err) {
-      return new Response('Inwalid color: ' + err, {status: 400})
-    }
-    return new Response(userColor.toString(), {status: 200})
+    r.get('.*/', (req) => addColor(decoded))
   } catch (err) {
     return new Response(err, {status: 403})
   }
+
+  const resp = await r.route(request)
+  return resp
+}
+
+async function addColor(decoded) {
+  console.log(decoded)
+  let userColor, colorType
+  if (decoded.color) {
+    colorType = 'CSS'
+  } else if (decoded.h || decoded.s || decoded.v) {
+    colorType = 'HSV'
+  } else if (decoded.r || decoded.g || decoded.b) {
+    colorType = 'RGB'
+  } else {
+    console.log(decoded)
+    return new Response('Invalid color in JWT ', { status: 400 })
+  }
+  try {
+    switch (colorType) {
+      case 'CSS':
+        userColor = color(decoded.color)
+        break
+      case 'HSV':
+        userColor = color({ h: decoded.h, s: decoded.s, v: decoded.v })
+        break
+      case 'RGB':
+        userColor = color({ r: decoded.r, g: decoded.g, b: decoded.b })
+    }
+  } catch (err) {
+    return new Response('Inwalid color: ' + err, { status: 400 })
+  }
+  return new Response(userColor.toString(), { status: 200 })
 }
 
